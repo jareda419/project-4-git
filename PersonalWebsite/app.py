c441ef2657e2b755a2506122e5335456f9a3c0bb
Jared <jared.alstrom@usu.edu>
2025-02-22 02:48:05 +0000

    Project4

---
+++ PersonalWebsite/app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask (__name__)
+
+@app.route("/")
+def hello_world():
+    return render_template("index.html")
+
+@app.route("/about")
+def about_me():
+    return render_template("about_me.html")
+
+@app.route("/favorite")
+def My_Favs():
+    return render_template("My-Favs.html")
+
